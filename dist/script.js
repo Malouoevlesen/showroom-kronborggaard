8456f3607bbe6dcd4956268aa6350620d963583e
Malou Øvlesen <malouoevlesen@gmail.com>
2019-03-01 09:35:46 +0100

    Made gallery page

---
+++ dist/script.js
@@ -0,0 +1,21 @@
+/* Gallery */ 
+
+var galleryTimer = setInterval(next, 2500);
+var index = 0;
+var newPhoto;
+
+function next(){
+	if(index<4){
+		index++;
+	}else{
+		index=0;
+	}
+	
+	change();
+
+}
+
+function change(){
+  newPhoto="images/gallery"+index+".jpg";
+  document.querySelector("#gallery").setAttribute("src", newPhoto);
+}
